5d57134576d8510aa014ac84a382e32176e718e9
Greg <gouwa5700@gmail.com>
2014-09-26 17:53:21 +0800

    add binding for Document

---
+++ middleware/kit_base_document.rb
@@ -0,0 +1,25 @@
+<%-
+n = "kit_base"
+N = "KitBase"
+
+a = "item"
+A = "Item"
+A_ = "Item"
+as = "items"
+As = "Items"
+a_name = "identifier"
+
+b = "document"
+B = "Document"
+B_ = "Document"
+bs = "documents"
+Bs = "Documents"
+b_attributes = ["id", "file", "path", "document_id", "description", "latest_version", "addendum", "updated_since_last_publish", "has_update", "created_at", "updated_at"]
+b_attribute_groups = {
+  "Basic" => ["id", "file", "path", "document_id", "description", "latest_version", "addendum", "updated_since_last_publish", "has_update", "created_at", "updated_at"]
+}
+
+NA = "KitBase::Item"
+NB = "KitBase::Document"
+
+-%>
