18caadcc286c149542efd24d9025d56fee3066dd
Greg <gouwa5700@gmail.com>
2014-09-27 13:13:42 +0800

    add binding for maintenances_document

---
+++ bindings/kit_base_maintenances_document.rb
@@ -0,0 +1,7 @@
+<%-
+namespace = "kit_base"
+parent = "maintenance"
+parent_name_attribute = "identifier"
+child = "maintenances_document"
+child_attributes = ["id", "maintenance_id", "document_id", "created_at", "updated_at"]
+-%>
