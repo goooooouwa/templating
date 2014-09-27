fb5fbce4bd388d0076f1e52fba1dcde58cda6ec3
Greg <gouwa5700@gmail.com>
2014-09-27 12:19:46 +0800

    add binding for ItemsDocument

---
+++ bindings/kit_base_items_document.rb
@@ -0,0 +1,7 @@
+<%-
+namespace = "kit_base"
+parent = "item"
+parent_name_attribute = "identifier"
+child = "items_document"
+child_attributes = ["id", "item_id", "document_id", "created_at", "updated_at"]
+-%>
