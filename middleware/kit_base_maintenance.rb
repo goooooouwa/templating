0d9bb92845ffca3336376f7aa9f2fa4937897320
Greg <gouwa5700@gmail.com>
2014-09-29 11:35:27 +0800

    update binding for maintenance

---
+++ middleware/kit_base_maintenance.rb
@@ -0,0 +1,25 @@
+<%-
+n = "kit_base"
+N = "KitBase"
+
+a = "item"
+A = "Item"
+A_ = "Item"
+as = "items"
+As = "Items"
+a_name = "identifier"
+
+b = "maintenance"
+B = "Maintenance"
+B_ = "Maintenance"
+bs = "maintenances"
+Bs = "Maintenances"
+b_attributes = ["id", "item_id", "employee_id", "company_id", "maintained_at", "cost", "note", "created_at", "updated_at"]
+b_attribute_groups = {
+  "Basic" => ["id", "item_id", "employee_id", "company_id", "maintained_at", "cost", "note", "created_at", "updated_at"]
+}
+
+NA = "KitBase::Item"
+NB = "KitBase::Maintenance"
+
+-%>
