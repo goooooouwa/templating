83d91f817c1c4b3e8112d5dff95f4e15265842d4
Greg <gouwa5700@gmail.com>
2014-12-01 21:17:57 +0800

    add document group binding

---
+++ middleware/tender_base_documents_document_group.rb
@@ -0,0 +1,25 @@
+<%-
+n = "tender_base"
+N = "TenderBase"
+
+a = "document"
+A = "Document"
+A_ = "Document"
+as = "documents"
+As = "Documents"
+a_name = "filename"
+
+b = "document_group"
+B = "DocumentGroup"
+B_ = "Document_group"
+bs = "document_groups"
+Bs = "DocumentGroups"
+b_attributes = ["id", "name", "description", "created_at", "updated_at"]
+b_attribute_groups = {
+  "Basic" => ["id", "name", "description", "created_at", "updated_at"]
+}
+
+NA = "TenderBase::Document"
+NB = "TenderBase::DocumentGroup"
+
+-%>
